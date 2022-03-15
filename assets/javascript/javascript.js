7d07cd1890c821686a45f78a06397f6a7fe5ed37
Brainybrian316 <brainybrian316@gmail.com>
2022-03-15 00:35:28 -0400

    testing out features

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,54 +1,143 @@
-// ! variables for my elements
-var highscore = document.getElementById('highscore')
-var timerEl = document.getElementById('timer')
-var introEl = document.getElementById('intro')
-var btnEl = document.getElementById('btn')
-var quizEl = document.getElementById('quiz')
-var questionEl = document.getElementById('question')
-var scoreIndicatorEl = document.getElementById('scoreIndicator')
-var finishEl = document.getElementById('finish')
-var recordsEl = document.getElementById('finish')
-var highScoresEl = document.getElementById('highScores')
-var resetEl = document.getElementById('reset')
-var clearScores = document.getElementById('clearScores')
-var answersEl = document.getElementsById('answersList')
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-// * questions array
-const questionsArray = [{
-        'question': 'String values must be enclosed within _____ when assigned to variables.',
-        // holds the answer choices in an array
-        'answer': ['commas', 'curly brackets', 'quotes', 'parenthesis'],
-        //  selects the correct answer based on position
-        'correct': 2
-    },
-    {
-        'question': 'Commonly used data types DO NOT include:',
-        'answer': ['alerts', 'numbers', 'strings', 'booleans'],
-        'correct': 0
-    },
-    {
-        'question': 'The condition in an if/else statement is enclosed within _____',
-        'answer': ['quotes', 'curly brackets', 'square brackets', 'parenthesis'],
-        'correct': 3
-    },
-    {
-        'question': 'Which tag is used to identify the keywards describing the site?',
-        'answer': ['text', 'http-equiv', 'content', 'name'],
-        'correct': 2
-    },
-    {
-        'question': 'What tag element will display a line across the screen?',
-        'answer': ['br', 'a', 'hr', 'line'],
-        'correct': 2
-    }
-]
+let shuffledQuestions, currentQuestionIndex
 
-function startQuiz() {
-    for (i = 0; i < 4; i++) {
-        let button = document.createElement("button")
-        button.innerHTML = answersEl[0].answer[i]
-        btnEl.appendChild(button);
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
 
-btnEl.addEventListener("click", startQuiz);
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [{
+        question: 'What is 2 + 2?',
+        answers: [{
+                text: '4',
+                correct: true
+            },
+            {
+                text: '22',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Who is the best YouTuber?',
+        answers: [{
+                text: 'Web Dev Simplified',
+                correct: true
+            },
+            {
+                text: 'Traversy Media',
+                correct: true
+            },
+            {
+                text: 'Dev Ed',
+                correct: true
+            },
+            {
+                text: 'Fun Fun Function',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'Is web development fun?',
+        answers: [{
+                text: 'Kinda',
+                correct: false
+            },
+            {
+                text: 'YES!!!',
+                correct: true
+            },
+            {
+                text: 'Um no',
+                correct: false
+            },
+            {
+                text: 'IDK',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'What is 4 * 2?',
+        answers: [{
+                text: '6',
+                correct: false
+            },
+            {
+                text: '8',
+                correct: true
+            }
+        ]
+    }
+]
