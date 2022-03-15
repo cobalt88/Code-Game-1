e994ca4379ad20f4c95a8b93a4d79083a7da4298
Brainybrian316 <brainybrian316@gmail.com>
2022-03-14 23:14:40 -0400

    added to all files updated buttons and testing different things

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,39 +11,44 @@ var recordsEl = document.getElementById('finish')
 var highScoresEl = document.getElementById('highScores')
 var resetEl = document.getElementById('reset')
 var clearScores = document.getElementById('clearScores')
+var answersEl = document.getElementsById('answersList')
 
 // * questions array
-const questions = [{
+const questionsArray = [{
         'question': 'String values must be enclosed within _____ when assigned to variables.',
         // holds the answer choices in an array
-        'answers': ['commas', 'curly brackets', 'quotes', 'parenthesis'],
+        'answer': ['commas', 'curly brackets', 'quotes', 'parenthesis'],
         //  selects the correct answer based on position
         'correct': 2
     },
     {
         'question': 'Commonly used data types DO NOT include:',
-        'answers': ['alerts', 'numbers', 'strings', 'booleans'],
+        'answer': ['alerts', 'numbers', 'strings', 'booleans'],
         'correct': 0
     },
     {
         'question': 'The condition in an if/else statement is enclosed within _____',
-        'answers': ['quotes', 'curly brackets', 'square brackets', 'parenthesis'],
+        'answer': ['quotes', 'curly brackets', 'square brackets', 'parenthesis'],
         'correct': 3
     },
     {
         'question': 'Which tag is used to identify the keywards describing the site?',
-        'answers': ['text', 'http-equiv', 'content', 'name'],
+        'answer': ['text', 'http-equiv', 'content', 'name'],
         'correct': 2
     },
     {
         'question': 'What tag element will display a line across the screen?',
-        'answers': ['br', 'a', 'hr', 'line'],
+        'answer': ['br', 'a', 'hr', 'line'],
         'correct': 2
     }
 ]
 
-function changeQuestion() {
-    questionEl.innerText = questionsArray[0].question;
+function startQuiz() {
+    for (i = 0; i < 4; i++) {
+        let button = document.createElement("button")
+        button.innerHTML = answersEl[0].answer[i]
+        btnEl.appendChild(button);
+    }
 }
 
-btnEl.addEventListener("click", changeQuestion);
+btnEl.addEventListener("click", startQuiz);
