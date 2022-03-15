9e6f5ff0d9bc8c394e880754dd3ea9c292b1ba47
Brainybrian316 <brainybrian316@gmail.com>
2022-03-14 21:25:58 -0400

    refactored html and added question array to the Js file

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,43 +1,49 @@
 // ! variables for my elements
-var questionEl = document.getElementById('question')
-var answersEl = document.getElementById('answers')
+var highscore = document.getElementById('highscore')
 var timerEl = document.getElementById('timer')
+var introEl = document.getElementById('intro')
 var btnEl = document.getElementById('btn')
+var quizEl = document.getElementById('quiz')
+var questionEl = document.getElementById('question')
+var scoreIndicatorEl = document.getElementById('scoreIndicator')
+var finishEl = document.getElementById('finish')
+var recordsEl = document.getElementById('finish')
+var highScoresEl = document.getElementById('highScores')
+var resetEl = document.getElementById('reset')
+var clearScores = document.getElementById('clearScores')
 
 // * questions array
-const questionPool = [{
-        'question': 'place question here',
-        'answers': ['A', 'B', 'C', 'D'],
-        'correctChoice': 3
+const questions = [{
+        'question': 'String values must be enclosed within _____ when assigned to variables.',
+        // holds the answer choices in an array
+        'answers': ['commas', 'curly brackets', 'quotes', 'parenthesis'],
+        //  selects the correct answer based on position
+        'correct': 2
     },
     {
-        'question': 'place question here',
-        'answers': ['A', 'B', 'C', 'D'],
-        'correctChoice': 1
+        'question': 'Commonly used data types DO NOT include:',
+        'answers': ['alerts', 'numbers', 'strings', 'booleans'],
+        'correct': 0
     },
     {
-        'question': 'place question here',
-        'answers': ['A', 'B', 'C', 'D'],
-        'correctChoice': 2
+        'question': 'The condition in an if/else statement is enclosed within _____',
+        'answers': ['quotes', 'curly brackets', 'square brackets', 'parenthesis'],
+        'correct': 3
     },
     {
-        'question': 'place question here',
-        'answers': ['A', 'B', 'C', 'D'],
-        'correctChoice': 0
+        'question': 'Which tag is used to identify the keywards describing the site?',
+        'answers': ['text', 'http-equiv', 'content', 'name'],
+        'correct': 2
     },
     {
-        'question': 'place question here',
-        'answers': ['A', 'B', 'C', 'D'],
-        'correctChoice': 1
-    },
+        'question': 'What tag element will display a line across the screen?',
+        'answers': ['br', 'a', 'hr', 'line'],
+        'correct': 2
+    }
 ]
 
-function startGame() {
-    btnEl.addEventListener("click", myQuestions =>
-        myQuestions.preventDefault(),
-        displayMyQuestions()
-    );
+function changeQuestion() {
+    questionEl.innerText = questionsArray[0].question;
 }
 
-
-startGame()
+btnEl.addEventListener("click", changeQuestion);
